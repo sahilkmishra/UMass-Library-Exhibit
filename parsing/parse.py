097c8583586366e35b8979769b8d9da630b12f2a
Ubuntu <ubuntu@ip-172-31-29-178.ec2.internal>
2019-05-28 15:41:23 +0000

    Finished parser to parse fields that are specifid in the README

--- parsing/parse.py
+++ parsing/parse.py
@@ -3,8 +3,31 @@ import json
 
 
 def main():
-    print("hello")
+    filename = sys.argv[1]
+    parseResult = []
+    with open(filename) as jsonFile:
+        jsonFromFile = json.load(jsonFile)
+        for item in jsonFromFile['response']['docs']:
+            dataToPreserve = {}
+            insert_if_available(dataToPreserve,item,'itemID')
+            insert_if_available(dataToPreserve,item,'title')
+            insert_if_available(dataToPreserve,item,'abstract')
+            insert_if_available(dataToPreserve,item,'dateDisplay')
+            insert_if_available(dataToPreserve,item,'year')
+            insert_if_available(dataToPreserve,item,'topic')
+            insert_if_available(dataToPreserve,item,'collection')
+            insert_if_available(dataToPreserve,item,'FacetName')
+            parseResult.append(dataToPreserve)
 
+    with open("result"+str(filename),"w+") as outFile:
+        json.dump(parseResult,outFile)
+
+    print("done")
+
+def insert_if_available(giveTo,takeFrom,key):
+    if key in takeFrom:
+        giveTo[key] = takeFrom[key]
 
 if __name__ == '__main__':
     main()
+

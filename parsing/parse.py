4f34a17ff32e360fc0f73265f097534effebfac6
slymodi <ubuntu@ip-172-31-29-178.ec2.internal>
2019-05-30 16:56:40 +0000

    pruning 1 link results which are just no links

--- parsing/parse.py
+++ parsing/parse.py
@@ -2,20 +2,28 @@ import sys
 import json
 
 LINKDATA = ['topic', 'collection', 'FacetName']
-TOPLEVELDATA = [ 'title', 'abstract', 'dateDisplay', 'year']
+TOPLEVELDATA = [ 'title',  'dateDisplay']
+TOPLEVELDATAINARRAY = ['abstract', 'year']
 
 def main():
     filename = sys.argv[1]
     parseResult = {}
+    years = {}
     with open(filename) as jsonFile:
         jsonFromFile = json.load(jsonFile)
         for item in jsonFromFile['response']['docs']:
             dataToPreserve = {}
             linkData = {}
             for key in TOPLEVELDATA:
-                insert_if_available(dataToPreserve,item,key)
+                insert_if_available(dataToPreserve,item,key,False)
+            for key in TOPLEVELDATAINARRAY:
+                insert_if_available(dataToPreserve,item,key,True)
             for key in LINKDATA:
-                insert_if_available(linkData,item,key)
+                insert_if_available(linkData,item,key,False)
+            if 'year' in item:
+                years[str(item['year'][0])] = 1
+            else:
+                years['undated'] = 1
             dataToPreserve['linkdata'] = sorted({x for v in linkData.itervalues() for x in v})
             parseResult[item['itemID']] = dataToPreserve
 
@@ -26,16 +34,23 @@ def main():
                 linkTable[link] = []
             linkTable[link].append(itemID)
 
-    finalResult = {'nodes': parseResult, 'links' : linkTable}
+    for linkID, links in linkTable.items():
+        if len(links) <= 1:
+            del linkTable[linkID]
+
+    finalResult = {'nodes': parseResult, 'links' : linkTable, 'years' : sorted(years.keys())}
 
     with open("result"+str(filename),"w+") as outFile:
         json.dump(finalResult,outFile)
 
     print("done")
 
-def insert_if_available(giveTo,takeFrom,key):
+def insert_if_available(giveTo,takeFrom,key,inArray):
     if key in takeFrom:
-        giveTo[key] = takeFrom[key]
+        if inArray:
+            giveTo[key] = takeFrom[key][0]
+        else:
+            giveTo[key] = takeFrom[key]
 
 if __name__ == '__main__':
     main()

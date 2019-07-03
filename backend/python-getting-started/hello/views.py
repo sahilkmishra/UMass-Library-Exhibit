e0c7ccd5ff68404bfed69482f679f8476ccf1a2e
Sahil <slymishra@gmail.com>
2019-07-03 11:59:49 -0400

    trying subtree

---
+++ backend/python-getting-started/hello/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from utils.parse import *
+
+from .models import Greeting
+
+# Create your views here.
+def index(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, "index.html")
+
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, "db.html", {"greetings": greetings})
+
+def search(request):
+    
+    return JsonResponse({"8":"D"})
